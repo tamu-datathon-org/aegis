40acafe81e7ebf11e5020f7b41bcca6c0fe6f6b2
Christopher Vu <christophervu03@gmail.com>
2023-09-25 10:50:44 -0500

    added auth route to upload resume api

--- pages/api/upload-url.ts
+++ pages/api/upload-url.ts
@@ -1,21 +1,19 @@
 import s3 from '../../utils/aws-config';
-import { NextApiRequest, NextApiResponse } from 'next'
-import { MongoDBSingleton } from '../../utils/db';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import nextConnect from 'next-connect'
+import { authenticatedRoute } from '../../libs/middleware'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
+const handler = nextConnect();
+
+handler.get(authenticatedRoute(async (req: VercelRequest, res: VercelResponse, tdUser) => {
     if(req.query.fileType == 'application/pdf') {
-        const db = await MongoDBSingleton.getInstance();
-        const collection = db.collection('applications');
-
-        console.log(req.query);
+        // const db = await MongoDBSingleton.getInstance();
+        // const collection = db.collection('applications');
 
         const post = await s3.createPresignedPost({
             Bucket: "td-2023-resumes",
             Fields: {
-            key: req.query.firstName + "_" + req.query.lastName + "_Resume" + ".pdf",
+            key: tdUser.authId + "_Resume" + ".pdf",
             'Content-Type': req.query.fileType,
             },
             Expires: 60, // seconds
@@ -23,10 +21,12 @@ export default async function handler(
             ['content-length-range', 0, 1048576], // up to 1 MB
             ],
         })
-        MongoDBSingleton.closeConnection();
+        // MongoDBSingleton.closeConnection();
         res.status(200).json(post);
     } else {
         res.status(500).json({ message: 'Error creating document' });
     }
 
-}
+}));
+
+export default handler;
