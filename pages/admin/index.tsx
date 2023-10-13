7c625c103fe3d1939ae4902fcc9d218ac271280f
WarrenWu4 <warrenweiwu04@gmail.com>
2023-10-12 20:16:11 -0500

    Added accept, delete, and reject features for each applicant (UNTESTED)

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -58,6 +58,48 @@ function Home(): JSX.Element {
     const filteredApplicants = applicants.filter((applicant) => {
         return (applicant.firstName.toLowerCase() + ' ' + applicant.lastName.toLowerCase()).includes(searchTerm.toLowerCase())
     });
+
+    const deleteApplicant = async (email: string) => {
+      try {
+        await fetch("/apply/api/admin/deleteApplicant", {
+          method: "POST",
+          body: JSON.stringify({email: email}),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      } catch(err) {
+        console.error(err)
+      }
+    }
+
+    const rejectApplicant = async (email: string) => {
+      try {
+        await fetch("/apply/api/admin/rejectApplicant", {
+          method: "POST",
+          body: JSON.stringify({email: email}),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    const acceptApplicant = async (email: string) => {
+      try {
+        await fetch("/apply/api/admin/acceptApplicant", {
+          method: "POST",
+          body: JSON.stringify({email: email}),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      } catch(err) {
+        console.error(err)
+      }
+    }
     
     return (
       <div>
@@ -71,6 +113,7 @@ function Home(): JSX.Element {
             <th>Grad</th>
             <th>Classification</th>
             <th>Application Status</th>
+            <th>Actions</th>
           </tr>
           {filteredApplicants.map((applicant) => (
             <tr key={applicant.email}>
@@ -80,6 +123,27 @@ function Home(): JSX.Element {
               <td>{applicant.anticipatedGradYear}</td>
               <td>{applicant.classification}</td>
               <td>{applicant.appStatus}</td>
+              <td>
+                <div style={{display: "flex", justifyContent: "space-evenly", columnGap: "8px"}}>
+                  <button type='button' style={{width: "24px", height: "24px"}} onClick={() => {acceptApplicant(applicant.email)}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                    </svg>
+                  </button>
+
+                  <button type='button' style={{width: "24px", height: "24px"}} onClick={() => {rejectApplicant(applicant.email)}}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                  </button>
+
+                  <button type='button' style={{width: "24px", height: "24px"}} onClick={() => deleteApplicant(applicant.email)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                    </svg>
+                  </button>
+                </div>
+              </td>
             </tr>
             
           ))}
---
+++ pages/api/admin/deleteApplicant.ts
@@ -0,0 +1,33 @@
+import { authenticatedRoute } from '../../../libs/middleware'
+import clientPromise from '../../../utils/db';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import nextConnect from 'next-connect'
+
+const handler = nextConnect();
+handler.delete(authenticatedRoute(async (req: VercelRequest, res: VercelResponse, tdUser) => {
+    if(tdUser?.isAdmin) {
+        let client;
+        try {
+            client = await clientPromise;
+            const db = client.db();
+            const data = req.body;
+
+            // make sure there's only 1 key in the body (email) and that it exists
+            if (data === null || data.email === null || Object.keys(data).length > 1) {
+                res.status(400).json({ message: 'No email provided or body has too many keys' });
+            }
+            else {
+                const result = await db.collection('applications').deleteOne({ email: data.email });
+                res.status(200).json({message: `Successfully delete ${data.email} applicant`, result: result});
+            }
+
+        } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error deleting applicant', error });
+        }
+    } else {
+        res.status(401).json({ message: 'Unauthorized access: You are not an admin fucker.' });
+    }
+}));  
+
+export default handler;
