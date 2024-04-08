434adb1c9c5509e3cfb7fd482e5e4dd356b28be5
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-04-08 13:36:58 -0500

    fixing critical resume issue - attempt 4.75

--- pages/application/index.tsx
+++ pages/application/index.tsx
@@ -259,8 +259,8 @@ function Home(): JSX.Element {
             // const filename = encodeURIComponent(file?.name || "");
             // const fileType = encodeURIComponent(file?.type || "");
 
-            const res = await fetch(`/apply/api/upload-url?fileType=${fileType}&firstName=${firstName}&lastName=${lastName}`)
-            const {fields} = await res.json()
+            // const res = await fetch(`/apply/api/upload-url?fileType=${fileType}&firstName=${firstName}&lastName=${lastName}`)
+            // const {fields} = await res.json()
 
             Object.entries({...fields, file}).forEach(([key, value]) => {
                 formData.append(key, value as string);
