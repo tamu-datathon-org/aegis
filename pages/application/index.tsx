f6bb6f31bbca50b065db0cfe6c54e63c61fc5a25
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-04-17 23:09:34 -0500

    Revert "Adding a routing timer after submitting an application"
    This reverts commit 3f7f118127c6f0c2cf9285146e23a71f3b96e31d.

--- pages/application/index.tsx
+++ pages/application/index.tsx
@@ -45,7 +45,6 @@ function Home(): JSX.Element {
   const [mlhQ2, setmlhQ2] = useState(false);
   const [mlhQ3, setmlhQ3] = useState(false);
   const [liabilityTerms, setLiabilityTerms] = useState(false);
-  const [readyTimer, setReadyTimer] = useState(false);
 
   const [resume, setResume] = useState<File | null>(null);
 
@@ -56,17 +55,6 @@ function Home(): JSX.Element {
       (window as any).location = '/auth/login?r=/apply';
     }
   }, [status])
-
-  useEffect(() => {
-    if(readyTimer) {
-      const timerId = setTimeout(() => {
-        (window as any).location = '/apply';
-      }, 4000);
-
-      return () =>clearTimeout(timerId);
-    }
-
-  }, [readyTimer]);
     
   useEffect(() => {
       const fetchApplication = async () => {
@@ -312,7 +300,6 @@ function Home(): JSX.Element {
 
       if(response.status == 201 && res.status == 200) {
         setToast({ text: 'Application received!', type: 'success', delay: 3000 });
-        setReadyTimer(true);
       } else {
         throw new Error();
       }
