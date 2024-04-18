b80619a2b4f37956e7b99891753a20c866cc4958
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2024-04-17 23:09:29 -0500

    Revert "fixing critical resume issue - attempt 2"
    This reverts commit 15925187a8fc9c75888cefdfdc459c7f7fad6841.

--- pages/api/upload-url.ts
+++ pages/api/upload-url.ts
@@ -1,32 +1,32 @@
-// import s3 from '../../utils/aws-config';
-// import {VercelRequest, VercelResponse} from '@vercel/node';
-// import nextConnect from 'next-connect'
-// import {authenticatedRoute} from '../../libs/middleware'
+import s3 from '../../utils/aws-config';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import nextConnect from 'next-connect'
+import { authenticatedRoute } from '../../libs/middleware'
 
-// const handler = nextConnect();
-//
-// // handler.get(authenticatedRoute(async (req: VercelRequest, res: VercelResponse, tdUser) => {
-// //     if (req.query.fileType == 'application/pdf') {
-// //         // const db = await MongoDBSingleton.getInstance();
-// //         // const collection = db.collection('applications');
-// //
-// //         // const post = await s3.createPresignedPost({
-// //         //     Bucket: "td-2023-resumes",
-// //         //     Fields: {
-// //         //         key: tdUser.authId + "_Resume" + ".pdf",
-// //         //         'Content-Type': req.query.fileType,
-// //         //     },
-// //         //     Expires: 60, // seconds
-// //         //     Conditions: [
-// //         //         ["content-length-range", 0, 1048576], // up to 1 MB
-// //         //     ],
-// //         // })
-// //         // MongoDBSingleton.closeConnection();
-// //         // res.status(200).json(post);
-// //     } else {
-// //         res.status(500).json({message: 'Error creating document'});
-// //     }
-// //
-// // }));
-//
-// export default handler;
+const handler = nextConnect();
+
+handler.get(authenticatedRoute(async (req: VercelRequest, res: VercelResponse, tdUser) => {
+    if(req.query.fileType == 'application/pdf') {
+        // const db = await MongoDBSingleton.getInstance();
+        // const collection = db.collection('applications');
+
+        const post = await s3.createPresignedPost({
+            Bucket: "td-2023-resumes",
+            Fields: {
+            key: tdUser.authId + "_Resume" + ".pdf",
+            'Content-Type': req.query.fileType,
+            },
+            Expires: 60, // seconds
+            Conditions: [
+            ['content-length-range', 0, 1048576], // up to 1 MB
+            ],
+        })
+        // MongoDBSingleton.closeConnection();
+        res.status(200).json(post);
+    } else {
+        res.status(500).json({ message: 'Error creating document' });
+    }
+
+}));
+
+export default handler;
